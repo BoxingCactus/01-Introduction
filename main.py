93fa1509991da718c40d2db1fb019c6a27d96032
Gratzer <napagrat@iu.edu>
2019-08-26 18:58:57 -0400

    This is practicing with the print command.

--- main.py
+++ main.py
@@ -5,4 +5,9 @@ import utils
 utils.check_version((3,7))
 utils.clear()
 
-print('Hello, Class!')
+print('Hello world! I am Nathan Gratzer!')
+print('My favorite game at the moment is Mother 3.')
+print('I am worried that all this programming will go over my head.')
+print('I am genuinely excited to design some games.')
+print('My user number is 11980987!')
+print('https://github.com/BoxingCactus')
